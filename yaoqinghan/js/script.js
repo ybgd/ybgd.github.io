e59c51b386b450da2f39386cfebd3e61adf5c0f7
ybgd <1641364786@qq.com>
2017-12-15 20:31:08 +0800

    邀请函

---
+++ yaoqinghan/js/script.js
@@ -0,0 +1,22 @@
+var swiper = new Swiper('.swiper-container', {
+    //direction: 'vertical',
+    //pagination: {
+    //    el: '.swiper-pagination',
+    //    clickable: true,
+    //}
+
+    //Slides的滑动方向，可设置水平(horizontal)或垂直(vertical)。
+    direction: 'vertical',
+    //分页器
+    pagination: {
+        el: '.swiper-pagination',
+        //让分页器支持点击
+        clickable: true,
+    },
+
+    // 前进按钮
+    navigation: {
+        nextEl: '.swiper-button-next',
+    },
+
+});
